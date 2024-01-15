2cf355f65c856d136b7cfffcbe05ff18a6b30419
Axel Brisse <axel.brisse@gmail.com>
2024-01-15 19:11:59 +0100

    MinMaxScaler

--- histogram.py
+++ histogram.py
@@ -4,15 +4,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from parse_args import parse_args
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 
 def get_numeric_columns(data):
     return data.select_dtypes(include=[np.float64])
 
 
-def normalize_columns(data, columns):
-    return pd.DataFrame(StandardScaler().fit_transform(data[columns]), columns=columns)
+def standardize_columns(data, columns):
+    return pd.DataFrame(MinMaxScaler().fit_transform(data[columns]), columns=columns)
 
 
 _, WIDTH = DIMENSIONS = (3, 5)
@@ -20,11 +20,11 @@ BINS = 12
 TITLE = "Histograms of Class Grades by Hogwarts House"
 
 data = parse_args("Show histograms of class grades by Hogwarts house.")
+standardized_data = standardize_columns(data, get_numeric_columns(data).columns)
+data["Average"] = standardized_data.mean(axis=1)
 grouped_data = data.groupby("Hogwarts House")
-normalized_data = normalize_columns(data, get_numeric_columns(data).columns)
-data["Average"] = normalized_data.mean(axis=1)
 fig = plt.figure(figsize=(20, 10))
-for i, (col, series) in enumerate(get_numeric_columns(data).items()):
+for i, col in enumerate(get_numeric_columns(data)):
     ax = plt.subplot2grid(DIMENSIONS, divmod(i, WIDTH))
     grouped_data[col].plot(kind="hist", alpha=0.5, bins=BINS, ax=ax)
     ax.set_xlabel(col)
