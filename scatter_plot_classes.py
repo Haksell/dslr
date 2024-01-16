97fd327ef666c8618a479eebf0bf8aa753223331
Axel Brisse <axel.brisse@gmail.com>
2024-01-16 01:45:37 +0100

    clean scatter_plot_classes.py

--- scatter_plot_classes.py
+++ scatter_plot_classes.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-# TODO: clean the scatter plots
-# TODO: scatter_plot_1v1
-
 import matplotlib.pyplot as plt
 import numpy as np
 from parse_args import parse_args
 
-data = parse_args("Show scatter plots of class grades by Hogwarts house.")
+TITLE = "Scatter Plot of Log-Transformed Features"
+
+data = parse_args("Show a scatter plot of classes means and standard deviations.")
 numeric_df = data.select_dtypes(include=[float])
 log_transformed_df = numeric_df.apply(lambda x: np.log(x + 1 - x.min()))
 log_means = log_transformed_df.mean()
@@ -17,7 +16,8 @@ plt.figure(figsize=(10, 6))
 plt.scatter(log_means, log_std_devs)
 plt.xlabel("Log Mean")
 plt.ylabel("Log Standard Deviation")
-plt.title("Scatter Plot of Log-Transformed Features")
+plt.title(TITLE)
+plt.gcf().canvas.setWindowTitle(TITLE)
 for i, txt in enumerate(log_transformed_df.columns):
     plt.annotate(txt, (log_means.iloc[i], log_std_devs.iloc[i]))
 plt.show()
