7c5b171738b843c68342452f4e42e8e4d98b62a4
Axel Brisse <axel.brisse@gmail.com>
2024-01-16 03:43:52 +0100

    pretty pair_plot.py

--- pair_plot.py
+++ pair_plot.py
@@ -7,7 +7,7 @@ from utils import HOUSE_COLORS, parse_args
 
 def abbreviate_column_names(data):
     def abbreviate_column_name(column):
-        return "".join(w[0] for w in column.split()) if " " in column else column[:3]
+        return "".join(w[0] for w in column.split()) if " " in column else column[:7]
 
     columns = list(data.select_dtypes(include=[float]).columns)
     abbreviations = {c: abbreviate_column_name(c) for c in columns}
@@ -17,14 +17,16 @@ def abbreviate_column_names(data):
 data = parse_args("Show a pair plot of Hogwarts classes")
 data = abbreviate_column_names(data)
 
-plt.figure(figsize=(20, 15))
-pair_plot = sns.pairplot(data, hue="Hogwarts House", palette=HOUSE_COLORS)
+pair_plot = sns.pairplot(
+    data, hue="Hogwarts House", palette=HOUSE_COLORS, height=2, aspect=1.5
+)
+pair_plot._legend.remove()
 
 for ax in pair_plot.axes.flatten():
     ax.set_xticklabels([])
     ax.set_yticklabels([])
-    ax.set_xlabel(ax.get_xlabel().split(" ")[0])
-    ax.set_ylabel(ax.get_ylabel().split(" ")[0])
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 plt.get_current_fig_manager().set_window_title("Pair Plot of Hogwarts Classes")
 plt.show()
